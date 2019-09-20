f1da0fab92c8c8d0bb6fbee3b24528a4c3bb5b31
MaudL <lealmaud@gmail.com>
2019-09-20 09:59:14 +0200

    first commit

---
+++ calculette.js
@@ -0,0 +1,36 @@
+var resultat1;
+var resultat2;
+var resultat3;
+var resultat4;
+
+
+function addition(chiffre1, chiffre2){
+	var resultat1 = chiffre1 + chiffre2;
+	if(chiffre1 + chiffre2){
+		alert(resultat);
+	}
+	else {
+		alert(error);
+	}
+}
+
+function soustraire(chiffre1, chiffre2){
+	var resultat2 = chiffre1 - chiffre2;
+	if(chiffre1 + chiffre2){
+		alert(error);
+	}
+}
+
+function diviser(chiffre1, chiffre2){
+	var resultat3 = chiffre1 / chiffre2;
+	if(chiffre1 / chiffre2){
+		alert(error);
+	}
+}
+
+function multiplier(chiffre1, chiffre2){
+	var resultat4 = chiffre1 *  chiffre2;
+	if(chiffre1 * chiffre2){
+		alert(error);
+	}
+}
